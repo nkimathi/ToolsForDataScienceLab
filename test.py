7ead18d0d69ab43fc9db868831e166bc29616812
nkimathi <104829576+nkimathi@users.noreply.github.com>
2022-06-01 16:07:11 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,11 +1,1 @@
-from matplotlib import pyplot as plt
-import numpy as np
-
-# Generate 50 random data points along 3 dimensions
-x, y, scale = np.random.randn(3, 50)
-fig, ax = plt.subplots()
-
-# Map each onto a scatterplot we'll create with Matplotlib
-ax.scatter(x=x, y=y, c=scale, s=np.abs(scale)*500)
-ax.set(title="Some random data, created with JupyterLab!")
-plt.show()
+print("My First Python Code on Git")
